e574a1d20e0ae6f2635521c265b43ed3ff08510f
kisanet shshay <shshaykisanet@gmail.com>
2025-09-08 09:57:41 +0300

    implemented todo list

---
+++ app/src/main/java/com/akirachix/todos/screens/PostsScreen.kt
@@ -0,0 +1,25 @@
+
+
+package com.akirachix.todos.screens
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.akirachix.todos.ui.theme.viewmodel.PostsViewModel // Import your ViewModel
+
+@Composable
+fun PostsScreen(viewModel: PostsViewModel) {
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Posts Screen - ViewModel instance: $viewModel")
+
+    }
+}
