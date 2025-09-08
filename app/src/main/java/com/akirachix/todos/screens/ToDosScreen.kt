d00c2e1ce0d366fc9d004c7cc26d011ae920229e
shalinedavida <shalinedavida@gmail.com>
2025-09-08 10:08:19 +0300

    Update ToDosScreen.kt

--- app/src/main/java/com/akirachix/todos/screens/ToDosScreen.kt
+++ app/src/main/java/com/akirachix/todos/screens/ToDosScreen.kt
@@ -9,7 +9,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Card // Using M3 Card
+import androidx.compose.material3.Card 
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.CircularProgressIndicator
