653874c6b101e28371f6eb13764c532cdfe47645
Victoria-Ogega <victoriaogega@gmail.com>
2025-09-08 09:53:09 +0300

    feat:added checkboxes

--- app/src/main/java/com/akirachix/todos/ui/theme/Screens/TodoScreen.kt
+++ app/src/main/java/com/akirachix/todos/ui/theme/Screens/TodoScreen.kt
@@ -1,22 +1,70 @@
 package com.akirachix.todos.ui.theme.Screens
 
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ListItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.akirachix.todos.model.TODO
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TodoScreen(todos: List<TODO>) {
-    LazyColumn {
-        items(todos) { todo ->
-            ListItem(
-                headlineContent = { Text(todo.title) },
-                supportingContent = { Text("Completed: ${todo.completed}") }
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("My Todo List") },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color(0xFFF7F3FC)
+                ),
+
             )
-            Divider()
+        },
+        modifier = Modifier.padding()
+    ) { paddingValues ->
+        LazyColumn(
+            contentPadding = paddingValues,
+        ) {
+            items(todos) { todo ->
+                Card(
+                    shape = RoundedCornerShape(12.dp),
+
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 8.dp)
+                        .padding(horizontal = 16.dp)
+                ) {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier.padding(16.dp)
+                    ) {
+                        Checkbox(
+                            checked = todo.completed,
+                            onCheckedChange = {}
+                        )
+                        Spacer(modifier = Modifier.width(12.dp))
+                        Text(todo.title)
+                    }
+                }
+            }
         }
     }
 }
