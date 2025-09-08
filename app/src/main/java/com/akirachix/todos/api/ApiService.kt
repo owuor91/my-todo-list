432c0bfbbb05e400d21e17b2f8d68a8433d640ac
esther <esthernnyamburaa@gmail.com>
2025-09-08 09:58:02 +0300

    created a branched an worked on a todo list

--- app/src/main/java/com/akirachix/todos/api/ApiService.kt
+++ app/src/main/java/com/akirachix/todos/api/ApiService.kt
@@ -2,8 +2,17 @@ package com.akirachix.todos.api
 
 import com.akirachix.todos.model.Todo
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface ApiService {
     @GET("/todos")
     suspend fun getTodos(): List<Todo>
+
+    @GET("/todos/{id}")
+    suspend fun getTodoById(@Path("id") id: Int): Todo
+
+
+
 }
+
+
---
+++ app/src/main/java/com/akirachix/todos/viewMOdels/TodoViewModel.kt
@@ -0,0 +1,35 @@
+package com.akirachix.todos.viewMOdels
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.akirachix.todos.api.ApiClient
+import com.akirachix.todos.api.ApiService
+import com.akirachix.todos.model.Todo
+import com.akirachix.todos.repository.todoRepository
+import kotlinx.coroutines.launch
+
+
+class TodoViewModel : ViewModel() {
+    // Create the repository inside the ViewModel
+    private val repository = todoRepository(
+        ApiClient.buildApiClient(ApiService:class.java)
+    )
+
+    val todos = mutableStateOf<List<Todo>>(emptyList())
+    val isLoading = mutableStateOf(false)
+    val errorMessage = mutableStateOf<String?>(null)
+
+    fun fetchTodos() {
+        isLoading.value = true
+        errorMessage.value = null
+        viewModelScope.launch {
+            try {
+                todos.value = repository.fetchTodos()
+            } catch (e: Exception) {
+                errorMessage.value = e.message
+            }
+            isLoading.value = false
+        }
+    }
+}
